b7364e5d2865bf631e9ef103d808052abac8d71c
lightwind98 <595221867@qq.com>
2019-08-22 16:28:07 +0800

    add github access

--- src/main/java/com/lightwind98/community/controller/AuthorizeController.java
+++ src/main/java/com/lightwind98/community/controller/AuthorizeController.java
@@ -1,4 +1,31 @@
 package com.lightwind98.community.controller;
 
+import com.lightwind98.community.dto.AccessTokenDTO;
+import com.lightwind98.community.dto.GithubUser;
+import com.lightwind98.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id("71958daff7654e01754f");
+        accessTokenDTO.setClient_secret("59dab7177de1838675b292d99329e5971964b705");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
 }
